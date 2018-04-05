4e0fdf750bed21040700723ce589c80341307a91
maciekwiso <maciekwiso@o2.pl>
2018-04-05 08:24:41 +0200

    added new keywords to ignore; better filtering logic

--- src/main/java/com/visosoft/tvshowinfo/service/impl/picviewer/ApartmentsLoader.java
+++ src/main/java/com/visosoft/tvshowinfo/service/impl/picviewer/ApartmentsLoader.java
@@ -105,9 +105,10 @@ public class ApartmentsLoader implements PicLoader {
             "stan deweloperski", "bieńczyce", "dzielnicy tonie", "stan: deweloperski", "wola justowska", "kliny borkowskie",
             "stare miasto", "lokalu: deweloperski", "wola duchacka", "prądnik czerwony", "mieszkania: dewe", "oferta dewelopera",
             "na i piętrze", "90-tych", "z 199", "metalowców", "wieliczka", "nowej huty", "na 1 piętrze", "wykończenia deweloperski",
-            "przy wawelu", "Nullo", "lokalu: do wykończenia", "w 195", "stanie deweloperskim", "dietla", "olsza", "nowa inwestycja",
+            "przy wawelu", "nullo", "lokalu: do wykończenia", "w 195", "stanie deweloperskim", "dietla", "olsza", "nowa inwestycja",
             "nowoczesna inwestycja", "50-tych", "ulicy lea", "piecyk gazowy", "nowej inwestycji", "inwestycja powstanie",
-            "ścisłym centrum", "piec dwufunkcyjny", "planowe zakończenie budowy", "z lat 60-tych", "nowe mieszkania");
+            "ścisłym centrum", "piec dwufunkcyjny", "planowe zakończenie budowy", "z lat 60-tych", "nowe mieszkania",
+            "oddany do użytku w 2019", "oddany do użytku w 2018", "kwartale 201", "budynku z 19", "IX piętrowym bloku");
 
     private boolean looksInteresting(String desc) {
         desc = desc.toLowerCase();
